ca4de2fae24cd258b104ea7081ed9991cd8ad9ee
twdpy <60778646+twdpy@users.noreply.github.com>
2023-03-14 11:46:27 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,7 +2,7 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '晓敏, 早上好',
     desc: `
       **{{date.DATA}}**
       
@@ -22,11 +22,7 @@ const TEMPLATE_CONFIG = [
       
       {{comprehensive_horoscope.DATA}}
       
-      ---
-      
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
+
       
       ---
       
