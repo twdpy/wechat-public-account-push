a1834aab128e9cfbd48de3ef0bd86350a95f2290
twdpy <60778646+twdpy@users.noreply.github.com>
2023-03-14 11:33:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,6 +7,8 @@
  */
 const USER_CONFIG = {
 
+  USE_PASSAGE: 'push-plus',
+  
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
@@ -19,11 +21,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '晓敏',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: '06d4e982a5624e9f852e062beebf9d45',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
